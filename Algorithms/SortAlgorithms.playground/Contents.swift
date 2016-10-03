d84be65d13e958707c4ee838cfc380b491fd4fa7
Nguyen Thanh Duc <DucDP@Georges-MacBook-Air.local>
2016-10-03 14:37:30 +0700

    Week 2 - 2 - Log - Login Form

---
+++ Algorithms/SortAlgorithms.playground/Contents.swift
@@ -0,0 +1,62 @@
+//Selection Sort
+func selectionSort(_ array: [Int]) -> [Int] {
+  guard array.count > 1 else { return array }
+  var a = array
+  
+  for i in 0..<a.count - 1 {
+    var min = i
+    for j in i+1..<a.count {
+      if a[j] < a[min]{
+        min = j
+      }
+    }
+    
+    if i != min {
+      swap(&a[i], &a[min])
+    }
+  }
+  return a
+}
+
+let arraySelectionSort = [16, 15, 55, 21, 88, 81, 53, 22, 27, 66, 61, 106, 55]
+selectionSort(arraySelectionSort)
+
+//Insertion Sort
+func insertionSort(_ array: [Int]) -> [Int] {
+  guard array.count > 1 else { return array }
+  var a = array
+  
+  for i in 1..<a.count {
+    var tmp = i
+    while tmp > 0 && a[tmp] < a[tmp - 1]{
+      swap(&a[tmp], &a[tmp - 1])
+      tmp -= 1
+    }
+  }
+  return a
+}
+
+let arrayInsertionSort = [16, 15, 55, 21, 88, 81, 53, 22, 27, 66, 61, 106, 55]
+insertionSort(arraySelectionSort)
+
+//Quick Sort
+func quickSort(_ array: [Int]) -> [Int] {
+  guard array.count > 1 else { return array }
+  var a = array
+  
+  let pivot = a[a.count/2]
+  let less = a.filter { $0 < pivot } //number smaller than pivot
+  let equal = a.filter { $0 == pivot } // equal to pivot
+  let greater = a.filter { $0 > pivot } // bigger than pivot
+  
+  return quickSort(less) + equal + quickSort(greater)
+}
+
+let arrayQuickSort = [16, 15, 55, 21, 88, 81, 53, 22, 27, 66, 61, 106, 55]
+quickSort(arrayQuickSort)
+
+
+
+
+
+
