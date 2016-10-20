62b815d256c23e8c7641fffeb46ff9aa33e45238
Nguyen Thanh Duc <DucDP@Georges-MacBook-Air.local>
2016-10-20 10:31:28 +0700

    w4-MyMapKit

---
+++ Week4/MyMapKit/MyMapKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMapKit
+//
+//  Created by Nguyen Thanh Duc on 10/20/16.
+//  Copyright © 2016 NguyenThanhDuc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
