67a06003f2ca5d1fa843eab09feed4b86e648dc0
Nguyen Thanh Duc <DucDP@Georges-MacBook-Air.local>
2016-10-20 11:55:47 +0700

    w4 - MyMapKit

--- Week4/MyMapKit/MyMapKit/ViewController.swift
+++ Week4/MyMapKit/MyMapKit/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet var mapView: MKMapView!
+  @IBOutlet var mapTypeSegment: UISegmentedControl!
+  @IBOutlet var searchTextField: UITextField!
+  
+  let locationManager = CLLocationManager()
+  let geocoder = CLGeocoder()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    locationManager.requestWhenInUseAuthorization()
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest
   }
 
   override func didReceiveMemoryWarning() {
@@ -20,6 +29,41 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  // change map type
+  @IBAction func mapTypeSegmentValueChange(_ sender: AnyObject) {
+    switch mapTypeSegment.selectedSegmentIndex {
+    case 0:
+      mapView.mapType = .standard
+    case 1:
+      mapView.mapType = .satellite
+    case 2:
+      mapView.mapType = .hybrid
+    default:
+      break
+    }
+  }
 
+}
+
+// MARK: UITextFieldDelegate
+extension ViewController: UITextFieldDelegate {
+  func textFieldDidEndEditing(_ textField: UITextField) {
+    guard let address = searchTextField.text else { return }
+    
+    geocoder.geocodeAddressString(address, completionHandler: { (placemarks, error) -> Void in
+      guard let placemarks = placemarks else { return }
+      
+      for placemark in placemarks {
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegion(center: (placemark.location?.coordinate)!, span: span)
+        self.mapView.setRegion(region, animated: true)
+      }
+    })
+  }
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    searchTextField.endEditing(true)
+    return true
+  }
 }
 
